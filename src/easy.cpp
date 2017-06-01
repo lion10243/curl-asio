da35746a4fcebf54ebd2edfe19904bf6ccd64a38
Ingo Meneikis <ingo.meneikis@idealo.de>
2017-06-01 10:50:45 +0200

    rollback fix

--- src/easy.cpp
+++ src/easy.cpp
@@ -486,7 +486,7 @@ void easy::handle_completion(const boost::system::error_code& err)
 
 	multi_registered_ = false;
 	io_service_.post(boost::bind(handler_, err));
-	handler_=0;
+	//handler_=0;
 }
 
 void easy::init()
