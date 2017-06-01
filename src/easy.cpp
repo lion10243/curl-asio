134180043c1b7d0be26991ceffd59c0e3258b5e0
Ingo Meneikis <ingo.meneikis@idealo.de>
2017-06-01 10:32:34 +0200

    for std::function use =0 instead of clear

--- src/easy.cpp
+++ src/easy.cpp
@@ -486,7 +486,7 @@ void easy::handle_completion(const boost::system::error_code& err)
 
 	multi_registered_ = false;
 	io_service_.post(boost::bind(handler_, err));
-	handler_.clear();
+	handler_=0;
 }
 
 void easy::init()
